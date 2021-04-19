8809dd8e10471407b7a33f6b9109fc8adca1335f
anatoli <anatoli@wsb-167-72a.icx.local>
2021-04-19 13:53:04 +0300

    rewrite with usages f string and used in test parametrize feature

--- greet_app/greet.py
+++ greet_app/greet.py
@@ -1,10 +1,9 @@
 def greet(name):
-    if name is None:
-        name = "my friend"
     if isinstance(name, str):
         if name.isupper():
-            return "HELLO {}!".format(name)
-    else:
-        if len(name) == 2:
-            return "Hello, " + " and ".join(name) + "."
-    return "Hello, {}.".format(name)
+            return f"HELLO {name}!"
+    elif isinstance(name, (list, tuple)):
+        return f"Hello, {' and '.join(name) if len(name) == 2 else ''}."
+    elif name is None:
+        name = "my friend"
+    return f"Hello, {name}."
--- tests/test_greet.py
+++ tests/test_greet.py
@@ -6,7 +6,7 @@ from greet_app.greet import greet
 def test_greet_with_name():
     data = "Bob"
     result = greet(data)
-    expected = "Hello, {}.".format(data)
+    expected = f"Hello, {data}."
     assert result == expected
 
 
@@ -20,12 +20,11 @@ def test_greet_without_name():
 def test_greet_with_uppercase_name():
     data = "JERRY"
     result = greet(data)
-    expected = "HELLO {}!".format(data)
+    expected = f"HELLO {data}!"
     assert result == expected
 
 
-def test_greet_with_two_names_from_list():
-    data = ["Jill", "Jane"]
-    result = greet(data)
-    expected = "Hello, Jill and Jane."
-    assert result == expected
+@pytest.mark.parametrize("test_input,expected", [(["Jill", "Jane"], "Hello, Jill and Jane."), (("Mike", "James"),
+                                                                                            "Hello, Mike and James.")])
+def test_greet_with_two_names_from_arr(test_input, expected):
+    assert greet(test_input) == expected
